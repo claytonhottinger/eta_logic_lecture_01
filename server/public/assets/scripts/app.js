d9bf6e1489b9b7d49aec12befa21f2d0371f297b
Clayton Hottinger <chottinger@Claytons-MacBook-Pro.local>
2016-01-04 16:03:49 -0600

    woohoo

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,3 +1,38 @@
+var meowCount = 0;
+var timerCount = 5000;
+var clickCount = 3;
+
 $(document).ready(function(){
-    console.log("jQuery loads just fine");
+    init();
+
 });
+
+function init(){
+
+    enable();
+
+    setInterval(checkCount, timerCount);
+
+    console.log("jQuery loads just fine");
+}
+
+function enable(){
+    $('button').on('click',plusCount);
+}
+
+function disable(){
+    $('button').off('click',plusCount);
+}
+
+function plusCount(){
+    meowCount ++;
+    if(meowCount%clickCount == 0){
+        console.log('Meow');
+    }
+}
+
+function checkCount (){
+    if(meowCount%clickCount == 0){
+        console.log('Meow');
+    }
+}
